3e4ead0547d527a0806e49ac60bd9994320f8b25
Jaxel Rojas <jaxelr@gmail.com>
2021-04-16 15:42:21 -0400

    This gonna work on the first try

---
+++ AppVeyorTest.cs
@@ -0,0 +1,32 @@
+using System.Data.SqlClient;
+using Xunit;
+
+namespace sqlserver2019buildtest
+{
+    public class AppVeyorTest
+    {
+        [Fact]
+        public void AppVeyorTest_Only()
+        {
+            var connection = new SqlConnection("Server=(local)\\SQL2019;Database=master;User ID=sa;Password=Password12!");
+            //var connection = new SqlConnection("Server=(local);Database=master;Trusted_Connection=true;");
+
+            connection.Open();
+
+            var command = connection.CreateCommand();
+
+            command.CommandText = "SELECT 1";
+
+            int result = 0;
+            using (var reader = command.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    result = reader.GetInt32(0);
+                }
+            }
+
+            Assert.Equal(1, result);
+        }
+    }
+}
