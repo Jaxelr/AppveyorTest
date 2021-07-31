cd46b428e0674bf7b68ceac0faa7b7fa26e0228b
Jaxel Rojas <jaxelr@gmail.com>
2021-07-31 18:29:46 -0400

    chore: added mysql service validation

--- AppVeyorTest.cs
+++ AppVeyorTest.cs
@@ -7,14 +7,41 @@ namespace sqlserver2019buildtest
     public class AppVeyorTest
     {
         [Fact]
-        public void AppVeyorTest_Only()
+        public void AppVeyorTest_SqlServer()
         {
             string connectionString = Environment.GetEnvironmentVariable("Sql_Connection");
 
             Console.WriteLine($"The environment variable Sql_Connection is defined as: {connectionString}");
 
             var connection = new SqlConnection(connectionString);
-            
+
+            connection.Open();
+
+            var command = connection.CreateCommand();
+
+            command.CommandText = "SELECT 1";
+
+            int result = 0;
+            using (var reader = command.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    result = reader.GetInt32(0);
+                }
+            }
+
+            Assert.Equal(1, result);
+        }
+
+        [Fact]
+        public void AppVeyorTest_MySql()
+        {
+            string connectionString = Environment.GetEnvironmentVariable("MySql_Connection");
+
+            Console.WriteLine($"The environment variable Sql_Connection is defined as: {connectionString}");
+
+            var connection = new MySql.Data.MySqlClient.MySqlConnection(connectionString);
+
             connection.Open();
 
             var command = connection.CreateCommand();
