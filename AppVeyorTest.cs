f0ce994525134255b9ab8ff1d10d72c42482a914
Jaxel Rojas <jaxelr@gmail.com>
2021-12-13 13:43:39 -0400

    feat: added postgresql connection test

--- AppVeyorTest.cs
+++ AppVeyorTest.cs
@@ -1,55 +1,77 @@
-using System;
+using Npgsql;
 using System.Data.SqlClient;
 using Xunit;
 
-namespace sqlserver2019buildtest
+namespace AppveyorTests;
+
+public class AppVeyorTest
 {
-    public class AppVeyorTest
+    [Fact]
+    public void AppVeyorTest_SqlServer()
     {
-        [Fact]
-        public void AppVeyorTest_SqlServer()
+        var connection = new SqlConnection("Server=127.0.0.1,1434;Database=master;User Id=sa;Password=P@ssword123;");
+
+        connection.Open();
+
+        var command = connection.CreateCommand();
+
+        command.CommandText = "SELECT 1";
+
+        int result = 0;
+        using (var reader = command.ExecuteReader())
         {
-            var connection = new SqlConnection("Server=127.0.0.1,1434;Database=master;User Id=sa;Password=P@ssword123;");
-
-            connection.Open();
-
-            var command = connection.CreateCommand();
-
-            command.CommandText = "SELECT 1";
-
-            int result = 0;
-            using (var reader = command.ExecuteReader())
+            while (reader.Read())
             {
-                while (reader.Read())
-                {
-                    result = reader.GetInt32(0);
-                }
+                result = reader.GetInt32(0);
             }
-
-            Assert.Equal(1, result);
         }
 
-        [Fact]
-        public void AppVeyorTest_MySql()
+        Assert.Equal(1, result);
+    }
+
+    [Fact]
+    public void AppVeyorTest_MySql()
+    {
+        var connection = new MySql.Data.MySqlClient.MySqlConnection("Server=127.0.0.1;Port=3308;Database=test;User Id=root;");
+
+        connection.Open();
+
+        var command = connection.CreateCommand();
+
+        command.CommandText = "SELECT 1";
+
+        int result = 0;
+        using (var reader = command.ExecuteReader())
         {
-            var connection = new MySql.Data.MySqlClient.MySqlConnection("Server=127.0.0.1;Port=3308;Database=test;User Id=root;");
-            
-            connection.Open();
-
-            var command = connection.CreateCommand();
-
-            command.CommandText = "SELECT 1";
-
-            int result = 0;
-            using (var reader = command.ExecuteReader())
+            while (reader.Read())
             {
-                while (reader.Read())
-                {
-                    result = reader.GetInt32(0);
-                }
+                result = reader.GetInt32(0);
             }
-
-            Assert.Equal(1, result);
         }
+
+        Assert.Equal(1, result);
+    }
+
+    [Fact]
+    public void AppVeyorTest_Postgres()
+    {
+        var connection = new NpgsqlConnection("Host=localhost;Port=5432;database=postgres;User ID=postgres;Password=postgres");
+
+        connection.Open();
+
+        var command = connection.CreateCommand();
+
+        command.CommandText = "SELECT 1";
+
+        int result = 0;
+        using (var reader = command.ExecuteReader())
+        {
+            while (reader.Read())
+            {
+                result = reader.GetInt32(0);
+            }
+        }
+
+        Assert.Equal(1, result);
     }
 }
